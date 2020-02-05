0ef449e87e0815fc4026341a2115b101da4ef5da
Ramy Carter <tbone425@msn.com>
2020-02-04 20:16:42 -0800

    added subtraction, multiplication, and division

--- js/calculator.js
+++ js/calculator.js
@@ -1,7 +1,28 @@
 var add = function(number1 , number2) {
     return number1 + number2
     }
+var number1 = parseInt(prompt("Enter a number:"))
+var number2 = parseInt(prompt("Enter another number:"))
+var result = add(number1, number2);
+    alert(result);
+var sub = function(number1 , number2) {
+    return number1 - number2
+    }
+var number1 = parseInt(prompt("Enter a number:"))
+var number2 = parseInt(prompt("Enter another number:"))
+var result = sub(number1, number2)
+    alert(result);
+var multiply = function(number1 , number2) {
+    return number1 * number2
+    }
+var number1 = parseInt(prompt("Enter a number:"))
+var number2 = parseInt(prompt("Enter another number:"))
+var result = multiply(number1, number2)
+    alert(result);
+var divide = function(number1 , number2) {
+        return number1 / number2
+        }
     var number1 = parseInt(prompt("Enter a number:"))
     var number2 = parseInt(prompt("Enter another number:"))
-    var result = add(number1, number2);
-    alert(result);
+    var result = divide(number1, number2)
+        alert(result);
